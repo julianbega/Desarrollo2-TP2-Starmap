faf70cb5cccaa8ff5199d99971c39b33a9f57fd5
Julian <Julian.begamilrad@gmail.com>
2021-04-04 21:31:01 -0300

    Solar System
    Planets with material
    planet orbits
    randomized planet start
    PointLight with variation 
    camera movment with numbers 
    6 sides skybox

---
+++ StarMap/Assets/Script/Camera.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public List<Transform> Planetas;
+    public GameObject Sun;
+    private int actualPlanetInSight;
+    private Vector3 NewCameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualPlanetInSight = 0;
+        NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+        NewCameraPosition.y += 10;
+        this.transform.position = NewCameraPosition;
+        Planetas[0] = Sun.transform;
+        Planetas[1] = GameObject.Find("Earth(Clone)").transform;
+        Planetas[2] = GameObject.Find("Neptune(Clone)").transform;
+        Planetas[3] = GameObject.Find("Uranus(Clone)").transform;
+        Planetas[4] = GameObject.Find("Mars(Clone)").transform;
+        Planetas[5] = GameObject.Find("Mercury(Clone)").transform;
+        Planetas[6] = GameObject.Find("Venus(Clone)").transform;
+        Planetas[7] = GameObject.Find("Saturn(Clone)").transform;
+        Planetas[8] = GameObject.Find("Jupiter(Clone)").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (actualPlanetInSight == 8)
+            {
+                actualPlanetInSight = 0;
+            }
+            else
+            {
+                actualPlanetInSight += 1;
+            }
+            
+            NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+            NewCameraPosition.y += 10;
+            this.transform.position = NewCameraPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+          
+            if (actualPlanetInSight == 0)
+            {
+                actualPlanetInSight = 8;
+            }
+            else 
+            {
+                actualPlanetInSight -= 1;
+            }
+            NewCameraPosition = Planetas[actualPlanetInSight].transform.position;
+            NewCameraPosition.y += 10;
+            this.transform.position = NewCameraPosition;
+            
+        }
+        switch (actualPlanetInSight)
+        {
+            case 1:
+                Planetas[1] = GameObject.Find("Earth(Clone)").transform;
+                break;
+            case 2:
+                Planetas[2] = GameObject.Find("Neptune(Clone)").transform;
+                break;
+            case 3:
+                Planetas[3] = GameObject.Find("Uranus(Clone)").transform;
+                break;
+            case 4:
+                Planetas[4] = GameObject.Find("Mars(Clone)").transform;
+                break;
+            case 5:
+                Planetas[5] = GameObject.Find("Mercury(Clone)").transform;
+                break;
+            case 6:
+                Planetas[6] = GameObject.Find("Venus(Clone)").transform;
+                break;
+            case 7:
+                Planetas[7] = GameObject.Find("Saturn(Clone)").transform;
+                break;
+            case 8:
+                Planetas[8] = GameObject.Find("Jupiter(Clone)").transform;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ StarMap/Assets/Script/SolarSystem.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+
+    public GameObject MainCam;
+
+    public int actualPlanetInSystem;
+    private int actualPlanetSelected = 8;
+    public GameObject[] PosiblePlanets;
+    public List<GameObject> ActivePlanets;
+    private bool lightIntencifying = true;
+    Light mylight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mylight = this.GetComponent<Light>();
+        List<int> selectedPlanets = new List<int>();
+        int random = 0;
+        
+
+        for (int i = 0; i < actualPlanetInSystem; i++)
+        {
+            do
+            {
+                random = Random.Range(0, PosiblePlanets.Length);
+            } while (selectedPlanets.Contains(random));
+            selectedPlanets.Add(random);
+            
+        }
+        for (int i = 0; i < actualPlanetInSystem; i++)
+        {
+            GameObject go = Instantiate(PosiblePlanets[selectedPlanets[i]]);
+            ActivePlanets.Add(go);
+        }
+        MainCam.transform.position = new Vector3(0.0f, 15.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mylight.intensity <= 0)
+        {
+            lightIntencifying = true;
+        }
+        if (mylight.intensity >= 10)
+        {
+            lightIntencifying = false;
+        }
+        if (lightIntencifying)
+        {
+            mylight.intensity += 1 * Time.deltaTime;
+        }
+        else 
+        {
+            mylight.intensity -= 1 * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && actualPlanetInSystem >= 1)
+        {
+            actualPlanetSelected = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && actualPlanetInSystem >= 2)
+        {
+            actualPlanetSelected = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && actualPlanetInSystem >= 3)
+        {
+            actualPlanetSelected = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && actualPlanetInSystem >= 4)
+        {
+            actualPlanetSelected = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5) && actualPlanetInSystem >= 5)
+        {
+            actualPlanetSelected = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6) && actualPlanetInSystem >= 6)
+        {
+            actualPlanetSelected = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7) && actualPlanetInSystem >= 7)
+        {
+            actualPlanetSelected = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8) && actualPlanetInSystem >= 8)
+        {
+            actualPlanetSelected = 7;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            actualPlanetSelected = 8;            
+        }
+
+        if (actualPlanetSelected == 8)
+        {
+            MainCam.transform.position = new Vector3(0.0f, 15.0f, 0.0f);
+        }
+        else 
+        {
+            MainCam.transform.position = ActivePlanets[actualPlanetSelected].transform.position + new Vector3(0.0f, 10.0f);
+        }
+       
+          
+        
+
+        //cam.transform.position = this.transform.position;
+    }
+}
